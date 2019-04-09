2addfc9c579b746ee110c847d1e4e77ae1669512
Diptanshu Kakwani <dipkakwani@gmail.com>
2019-04-09 15:22:14 +0530

    add train

--- src/main/scala/com/iitm/research_explorer/PaperRank.scala
+++ src/main/scala/com/iitm/research_explorer/PaperRank.scala
@@ -1,25 +1,28 @@
 package com.iitm.research_explorer
 
-/*
-  Graph Structure:
-    Vertex
-      - Type
-      - Authors
-      - Papers
-      - Venues
-      -
- */
-object PaperRank {
+import org.graphframes.GraphFrame
+import org.graphframes.lib.Pregel
+import org.apache.spark.sql.functions._
 
-//  //val graph: GraphFrame
-//  val numPapers = 1000
-//  val numAuthors = 1000
-//  val numVenues = 1000
-//
-//  val ranks = graph.pregel
-//    .withVertexColumn("rank", lit(1.0 / (numPapers + numAuthors + numVenues)),
-//      coalesce(Pregel.msg, lit(0.0)) + Pregel.src("rank"))
-//    .sendMsgToDst(Pregel.src("rank") / Pregel.src("outDegree"))
-//    .aggMsgs(sum(Pregel.msg))
-//    .run()
+/*
+*
+* Rank authors, paper and venues.
+* Based on "A Graph Analytics Framework for Ranking Authors, Papers and Venues"
+* [https://arxiv.org/pdf/1708.00329.pdf]
+*
+* Uses GraphFrame's Pregel API:
+* https://graphframes.github.io/graphframes/docs/_site/api/scala/index.html#org.graphframes.lib.Pregel
+* Implementation ideas:
+* Initialize rank with the degree of each vertex
+* Send change in rank (newRank - prevRank) as update to the connected edges.
+
+ */
+class PaperRank (graph: GraphFrame) {
+
+  val ranks = graph.pregel
+    .withVertexColumn("rank",  lit(1.0 / (numPapers + numAuthors + numVenues)),
+      coalesce(Pregel.msg, lit(0.0)) + Pregel.src("rank"))
+    .sendMsgToDst(Pregel.src("rank") / Pregel.src("outDegree"))
+    .aggMsgs(sum(Pregel.msg))
+    .run()
 }
