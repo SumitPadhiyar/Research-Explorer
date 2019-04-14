2cc3d6253192f1f5dd14fd902c2a3c1be2898377
Diptanshu Kakwani <dipkakwani@gmail.com>
2019-04-14 11:50:02 +0530

    modifications in paper rank

--- src/main/scala/com/iitm/research_explorer/PaperRank.scala
+++ src/main/scala/com/iitm/research_explorer/PaperRank.scala
@@ -13,16 +13,27 @@ import org.apache.spark.sql.functions._
 * Uses GraphFrame's Pregel API:
 * https://graphframes.github.io/graphframes/docs/_site/api/scala/index.html#org.graphframes.lib.Pregel
 * Implementation ideas:
+* We keep track of rank of each vertex. Whenever there is a change in rank of the vertex,
+* it propagates the change to its neighbor in the form of delta (current_rank - previous_rank).
+* This delta can then simply be added by each vertex to its own rank.
 * Initialize rank with the degree of each vertex
-* Send change in rank (newRank - prevRank) as update to the connected edges.
+* To compute the delta, we keep track of both the current and previous rank.
+* Initially previous_rank = 1 and current_rank = 1 / deg(v)
 
  */
 class PaperRank (graph: GraphFrame) {
 
   val ranks = graph.pregel
-    .withVertexColumn("rank",  lit(1.0 / (numPapers + numAuthors + numVenues)),
-      coalesce(Pregel.msg, lit(0.0)) + Pregel.src("rank"))
-    .sendMsgToDst(Pregel.src("rank") / Pregel.src("outDegree"))
+    // Column previous_rank. After the messages are received, it stores the current
+    // value of rank
+    .withVertexColumn("prev_rank", lit(1.0), Pregel.src("rank"))
+    // Column current rank. Normalizes the rank after adding all the messages
+    .withVertexColumn("rank",  lit(lit(1.0) / Pregel.src("degree")),
+      (coalesce(Pregel.msg, lit(0.0)) + Pregel.src("rank"))
+        /Pregel.src("degree"))
+    // TODO: Send messages to neighbors only when the current rank is different from previous rank
+    .sendMsgToDst(Pregel.src("rank") - Pregel.src("prev_rank"))
+    // Sum messages received from the neighbors
     .aggMsgs(sum(Pregel.msg))
     .run()
 }
